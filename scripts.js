92031ad7e04bc5e6c314539dd710bdaf3c83703e
pccfpp <filippo_piccardi@yahoo.it>
2017-09-13 16:20:52 +0200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var todos = ['item 1', 'item 2', 'item 3']; //it stores todos
+
+console.log('My todos', todos); //it displays todos
+
+todos.push('new todo'); //it adds new todos
+
+todos[0] = 'Changed!'; //it changes todos
+
+todos.splice(0, 1); //it deletes todos
+
